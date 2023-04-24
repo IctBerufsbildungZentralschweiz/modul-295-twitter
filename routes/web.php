f65121b8c9456654f45f78e8be42414547df0091
Tobias Kündig <tobias@offline.ch>
2023-04-24 16:03:42 +0200

    fallback Route korrigiert

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::fallback( function () {
     if (!str_starts_with(request()->path(), 'api')) {
-        return file_get_contents(public_path('index.html'));
+        return response()->json(['error' => 'Not Found'], 404);
     }
 
-    return response()->json(['error' => 'Not Found'], 404);
+    return file_get_contents(public_path('index.html'));
 });
