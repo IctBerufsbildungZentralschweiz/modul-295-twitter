e3254c41ccd64b3bdf32de0bf83d68e470c21edc
Patrick <patrick@offline.ch>
2023-12-22 11:43:51 +0100

    Texte für Logout-Tests korrigiert

--- tests/Feature/F6Test.php
+++ tests/Feature/F6Test.php
@@ -12,7 +12,7 @@ class F6Test extends TestCase
     use RefreshDatabase;
     protected $seed = true;
 
-    public function test_endpoint_get_logout_returns_200_on_logout_success(): void
+    public function test_endpoint_post_logout_returns_200_on_logout_success(): void
     {
         Sanctum::actingAs(User::factory()->create());
         $response = $this->postJson('/api/logout');
@@ -22,13 +22,13 @@ class F6Test extends TestCase
         $response->assertJsonStructure(['message']);
     }
 
-    public function test_endpoint_get_logout_returns_200_on_logout_fail(): void
+    public function test_endpoint_post_logout_returns_200_on_logout_fail(): void
     {
         $response = $this->postJson('/api/logout');
         $response->assertStatus(401);
     }
 
-    public function test_endpoint_get_logout_returns_401_with_logged_out_user(): void
+    public function test_endpoint_post_logout_returns_401_with_logged_out_user(): void
     {
         Sanctum::actingAs(User::factory()->create());
         $this->postJson('/api/logout');
