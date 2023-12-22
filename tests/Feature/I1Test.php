08684547f644646e1af66ad4940215b33bed6dd7
Patrick <patrick@offline.ch>
2023-12-22 14:28:05 +0100

    Integrity-Test für SQLite umgeschrieben

--- tests/Feature/I1Test.php
+++ tests/Feature/I1Test.php
@@ -48,7 +48,7 @@ class I1Test extends TestCase
 
     public function test_endpoint_get_users_id_returns_positive_is_valid_if_likes_more_than_100000()
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        DB::statement('PRAGMA foreign_keys = OFF;');
 
         $user = User::factory()->has(
             Tweet::factory()->count(1)->state([
