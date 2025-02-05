570dee5e31adab57bdf58f6f411d1181db9a7ad2
Patrick <patrick@offline.ch>
2025-02-05 17:23:25 +0100

    Init

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::fallback( function () {
     if (str_starts_with(request()->path(), 'api')) {
         return response()->json(['error' => 'Not Found'], 404);
